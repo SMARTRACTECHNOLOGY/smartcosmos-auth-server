9201c1a81ae0b151fa999d0a6c2eac356c1e39da
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-01-10 12:16:18 +0100

    Apply code style

--- src/test/java/net/smartcosmos/cluster/auth/AuthServerApplicationTest.java
+++ src/test/java/net/smartcosmos/cluster/auth/AuthServerApplicationTest.java
@@ -1,13 +1,20 @@
 package net.smartcosmos.cluster.auth;
 
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,16 +22,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-/**
- * @author voor
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AuthServerApplication.class)
 @WebAppConfiguration
@@ -39,43 +38,51 @@ public class AuthServerApplicationTest {
 
     @Test
     public void homePageProtected() {
+
         ResponseEntity<String> response = template.getForEntity("http://localhost:"
-            + port, String.class);
+                                                                + port, String.class);
         assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test
     public void authorizationRedirects() {
+
         ResponseEntity<String> response = template.getForEntity("http://localhost:"
-            + port + "/oauth/authorize", String.class);
+                                                                + port + "/oauth/authorize", String.class);
         assertEquals(HttpStatus.FOUND, response.getStatusCode());
-        String location = response.getHeaders().getFirst("Location");
+        String location = response.getHeaders()
+            .getFirst("Location");
         assertTrue("Wrong header: " + location,
-            location.startsWith("http://localhost:" + port + "/login"));
+                   location.startsWith("http://localhost:" + port + "/login"));
     }
 
     @Test
     public void loginSucceeds() {
+
         ResponseEntity<String> response = template.getForEntity("http://localhost:"
-            + port + "/login", String.class);
+                                                                + port + "/login", String.class);
         String csrf = getCsrf(response.getBody());
-        MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
         form.set("username", "user");
         form.set("password", "password");
         form.set("_csrf", csrf);
         HttpHeaders headers = new HttpHeaders();
-        headers.put("COOKIE", response.getHeaders().get("Set-Cookie"));
+        headers.put("COOKIE",
+                    response.getHeaders()
+                        .get("Set-Cookie"));
 
         RequestEntity<MultiValueMap<String, String>> request = new RequestEntity<>(
             form, headers, HttpMethod.POST, URI.create("http://localhost:" + port
-            + "/login"));
+                                                       + "/login"));
         ResponseEntity<Void> location = template.exchange(request, Void.class);
         assertEquals(HttpStatus.FOUND, location.getStatusCode());
         assertEquals("http://localhost:" + port + "/",
-            location.getHeaders().getFirst("Location"));
+                     location.getHeaders()
+                         .getFirst("Location"));
     }
 
     private String getCsrf(String soup) {
+
         Matcher matcher = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*")
             .matcher(soup);
         if (matcher.matches()) {
